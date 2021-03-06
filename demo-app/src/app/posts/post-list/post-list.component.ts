dc08f8576f1eac1dabbf0313c4675918bd58d77b
BigMac <bigmac@BigMac.local>
2021-03-06 22:22:10 +1100

    ng Loop

---
+++ demo-app/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+  posts = [
+    // {title: 'First post', content: '1111111'},
+    // {title: 'Second post', content: '222'},
+    // {title: 'Third post', content: '3333'},
+  ]
+}
